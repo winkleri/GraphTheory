f4513c657db3eead418d9e3abcc1cf79377b3576
ng7211 <demy.abdulsalami@haw-hamburg.de>
2025-11-04 00:36:57 +0100

    graphgenerator as class variable

--- GraphConceptsAndAlgorithms/src/main/java/Main.java
+++ GraphConceptsAndAlgorithms/src/main/java/Main.java
@@ -5,13 +5,13 @@ import java.awt.*;
 import java.util.Scanner;
 
 public class Main {
+    private static GraphGenerator gg = new GraphGenerator();
     //Entry point
     static void main() {
-        GraphGenerator gg = new GraphGenerator();
         //Initialize parser with same generator instance
         GraphParser gp = new GraphParser(gg);
         gp.fileParser(gp.checkFiles());
-        initializeTextBasedUI(gg);
+        initializeTextBasedUI();
     }
 
     /**
@@ -36,23 +36,13 @@ public class Main {
      *
      * @param gg Graph generator object
      */
-    public static void initializeTextBasedUI(GraphGenerator gg) {
+    public static void initializeTextBasedUI() {
         System.out.printf("%d graphs found\n", gg.getImmutableGraphs().size());
         for (int i = 0; i < gg.getImmutableGraphs().size(); i++) {
             Graph g = gg.getImmutableGraphs().get(i);
             System.out.printf("\n %d - %s", i, g.getAttribute("sourceFile"));
         }
-        uiInteraction(gg);
-    }
 
-    /**
-     * This method allows a user to interact with the program.
-     * The user can choose between option 1 and option 2.
-     * Option 1: chosen graph is visualized
-     * Option 2: The BFS algorithm will be performed on the chosen graph.
-     * @param gg is a variable to generate a graph.
-     */
-    public static void uiInteraction(GraphGenerator gg) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\n\nPress 0 if you want to visualize a graph\nPress 1 if you want to use the BFS algorithm\n");
@@ -66,15 +56,25 @@ public class Main {
             case "1":
                 System.out.println("\nEnter the number of the graph that you want the BFS algorithm to be performed on");
                 int graphNum = Integer.parseInt(scanner.nextLine());
+
                 System.out.println("\nEnter the ID of the source node");
-                String sourceID = gg.checkNodeID(gg.getImmutableGraphs().get(graphNum), scanner.nextLine());
+                String sID = scanner.nextLine();
+                String sourceID = "";
+                if (gg.containsNodeId(gg.getMutableGraphs().get(graphNum), sID)) {
+                    sourceID = sID;
+                }
+
                 System.out.println("\nEnter the ID of the target node");
-                String targetID = gg.checkNodeID(gg.getImmutableGraphs().get(graphNum), scanner.nextLine());
+                String tID = scanner.nextLine();
+                String targetID = "";
+                if (gg.containsNodeId(gg.getImmutableGraphs().get(graphNum), tID)) {
+                    targetID = tID;
+                }
 
                 BreadthFirstSearch bfs = new BreadthFirstSearch(sourceID, targetID);
                 bfs.init(gg.getImmutableGraphs().get(graphNum));
                 bfs.compute();
-                System.out.println(bfs.printPath());
+                System.out.println(bfs.toString());
                 break;
             default:
                 System.out.println("Invalid input");
